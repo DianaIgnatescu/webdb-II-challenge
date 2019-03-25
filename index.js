81b3691bdda38b3ec3dc0f0fe0dcde454e7a3284
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-03-25 19:47:51 +0000

    MVP: Create endpoint to insert zoos

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+const knexConfig = require('./knexfile');
+
+const db = knex(knexConfig.development);
 
 const server = express();
 
@@ -7,6 +11,32 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+server.post('/api/zoos', (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    res.status(400).json({ errorMessage: 'Please provide a name for the zoo.' });
+  } else {
+    db.insert({name})
+        .into('zoos')
+        .then(ids => {
+          res.status(201).json(ids);
+        })
+        .catch(error => {
+          res.status(500).json({ errorMessage: 'The zoo could not be created. '});
+        });
+  }
+});
+
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+      .then((zoos) => {
+        res.status(200).json(zoos);
+      })
+      .catch((error) => {
+        res.status(500).json({ errorMessage: 'The zoos could not be retrieved.' });
+      })
+});
 
 const port = 3300;
 server.listen(port, function() {
