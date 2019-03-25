c59b5e842d7bb0005f2117b0363273dc913db2d5
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-03-25 20:25:55 +0000

    MVP: Create endpoint to update zoo

--- index.js
+++ index.js
@@ -18,53 +18,71 @@ server.post('/api/zoos', (req, res) => {
     res.status(400).json({ errorMessage: 'Please provide a name for the zoo.' });
   } else {
     db.insert({name})
-        .into('zoos')
-        .then(ids => {
-          res.status(201).json(ids);
-        })
-        .catch(error => {
-          res.status(500).json({ errorMessage: 'The zoo could not be created. '});
-        });
+      .into('zoos')
+      .then(ids => {
+        res.status(201).json(ids);
+      })
+      .catch(error => {
+        res.status(500).json({ errorMessage: 'The zoo could not be created. '});
+      });
   }
 });
 
 server.get('/api/zoos', (req, res) => {
   db('zoos')
-      .then((zoos) => {
-        res.status(200).json(zoos);
-      })
-      .catch((error) => {
-        res.status(500).json({ errorMessage: 'The zoos could not be retrieved.' });
-      })
+    .then((zoos) => {
+      res.status(200).json(zoos);
+    })
+    .catch((error) => {
+      res.status(500).json({ errorMessage: 'The zoos could not be retrieved.' });
+    })
 });
 
 server.get('/api/zoos/:id', (req, res) => {
   const {id} = req.params;
   db('zoos')
-      .where({ id: id })
-      .then((zoo) => {
-      if(!zoo) {
-        res.status(404).json({ message: 'The zoo with the specified ID does not exist.' });
-      } else {
-        res.status(200).json(zoo)
-      }
-      })
-      .catch((error) => {
-        res.status(500).json({ error: 'The zoo information could not be retrieved.' });
-      });
+    .where({ id: id })
+    .then((zoo) => {
+    if(!zoo) {
+      res.status(404).json({ message: 'The zoo with the specified ID does not exist.' });
+    } else {
+      res.status(200).json(zoo)
+    }
+    })
+    .catch((error) => {
+      res.status(500).json({ error: 'The zoo information could not be retrieved.' });
+    });
 });
 
 server.delete('/api/zoos/:id', (req, res) => {
   const { id } = req.params;
   db('zoos')
+    .where({ id: id })
+    .del()
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    });
+});
+
+server.put('/api/zoos/:id', (req, res) => {
+  const { id } = req.params;
+  const zoo = req.body;
+  if (!zoo) {
+    res.status(400).json({ errorMessage: 'Please provide a name for the zoo.' });
+  } else {
+    db('zoos')
       .where({ id: id })
-      .del()
-      .then(count => {
-        res.status(200).json(count);
+      .update(zoo)
+      .then((count) => {
+        res.status(200).json({ count });
       })
-      .catch((error) => {
-        res.status(500).json(error);
+      .catch(error => {
+        res.status(500).json({ errorMessage: 'The zoo information could not be modified.' })
       });
+  }
 });
 
 const port = 3300;
