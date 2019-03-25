73c1addd26316d0ccc8495ebb7b91bc75aeae9b2
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-03-25 20:03:58 +0000

    MVP: Create endpoint to get zoos and get zoo by id

--- index.js
+++ index.js
@@ -38,6 +38,22 @@ server.get('/api/zoos', (req, res) => {
       })
 });
 
+server.get('/api/zoos/:id', (req, res) => {
+  const {id} = req.params;
+  db('zoos')
+      .where({ id: id })
+      .then((zoo) => {
+      if(!zoo) {
+        res.status(404).json({ message: 'The zoo with the specified ID does not exist.' });
+      } else {
+        res.status(200).json(zoo)
+      }
+      })
+      .catch((error) => {
+        res.status(500).json({ error: 'The zoo information could not be retrieved.' });
+      });
+});
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
